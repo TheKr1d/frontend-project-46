2663fcfa5a5864bf743900b68a3c40d7eed3e976
thekr1d <epifanov.vv.spb@gmail.com>
2026-04-06 21:31:58 +0300

    ad fix hexlet check 5.0

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,6 +10,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .exitOverride()
 
 program
   .argument('<filepath1>')
