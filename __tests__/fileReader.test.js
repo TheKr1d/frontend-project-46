7feaa1a97841e5584838275f364d326965d08fa9
thekr1d <epifanov.vv.spb@gmail.com>
2026-04-06 20:53:35 +0300

    remove fileReader.js

--- __tests__/fileReader.test.js
+++ __tests__/fileReader.test.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { expect, test } from '@jest/globals'
-import { reader } from '../src/fileRader.js'
+import { reader } from '../src/parsers'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
