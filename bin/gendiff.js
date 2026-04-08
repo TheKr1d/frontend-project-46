ac7cd25196841a95714ca44b4512d985ff82293c
thekr1d <epifanov.vv.spb@gmail.com>
2026-04-08 09:01:24 +0300

    fix bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
-import { program } from 'commander'
+import { Command } from 'commander'
 import genDiff from '../src/index.js'
 import parser, { reader } from '../src/parsers.js'
 
 const formats = ['stylish', 'plain', 'json']
+
+const program = new Command()
 
 program
   .name('gendiff')
@@ -12,19 +14,15 @@ program
   .version('0.0.1')
 
 program
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .option('-f, --format <type>', `output format [${formats.join(', ')}]`, 'stylish')
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+  .option('-f, --format [type]', `output format [${formats.join(', ')}]`, 'stylish')
   .action((filepath1, filepath2, options) => {
     const format = options.format
-    if (!formats.includes(format)) {
-      console.log(`Unknown format: ${format}. Use option --help`)
-      process.exit(1)
-    }
     const parseFile1 = parser(reader(filepath1), filepath1)
     const parseFile2 = parser(reader(filepath2), filepath2)
 
     console.log(genDiff(parseFile1, parseFile2, format))
   })
 
-program.parse()
+program.parse(process.argv)
