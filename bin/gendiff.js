6c0c121b114acfe156af35fb77c72ce765ad008a
thekr1d <epifanov.vv.spb@gmail.com>
2026-04-08 09:26:02 +0300

    fix hexlet test

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,21 +3,14 @@
 import { Command } from 'commander'
 import genDiff from '../src/index.js'
 
-const formats = ['stylish', 'plain', 'json']
-
 const program = new Command()
 
 program
-  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-
-program
-  .argument('<filepath1>', 'path to first file')
-  .argument('<filepath2>', 'path to second file')
-  .option('-f, --format [type]', `output format [${formats.join(', ')}]`, 'stylish')
-  .action((filepath1, filepath2, options) => {
-    console.log(genDiff(filepath1, filepath2, options.format))
-  })
+  .argument('filePath1', 'path to first file')
+  .argument('filePath2', 'path to second file')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filePath1, filePath2, options) => { console.log(genDiff(filePath1, filePath2, options.format)) })
 
 program.parse(process.argv)
