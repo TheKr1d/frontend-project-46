4667aeb333300d425dad636f69a4381e21eb08f0
thekr1d <epifanov.vv.spb@gmail.com>
2026-04-09 08:56:44 +0300

    I added styles to the commander and added the default format to gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,12 +5,14 @@ import genDiff from '../src/index.js'
 
 const program = new Command()
 
+const formats = ['stylish', 'plain', 'json']
+
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .argument('filePath1', 'path to first file')
   .argument('filePath2', 'path to second file')
-  .option('-f, --format [type]', 'output format', 'stylish')
+  .option('-f, --format [type]', `output format [${formats.join(', ')}]`, 'stylish')
   .action((filePath1, filePath2, options) => { console.log(genDiff(filePath1, filePath2, options.format)) })
 
 program.parse(process.argv)
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import formatter from './formatters/index.js'
 import buildDiffTree from './buildDiffTree.js'
 import parser, { reader } from './parsers.js'
 
-const genDiff = (filepath1, filepath2, format) => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const parseFile1 = parser(reader(filepath1), filepath1)
   const parseFile2 = parser(reader(filepath2), filepath2)
   const diffTree = buildDiffTree(parseFile1, parseFile2)
